4dbfa751a606a211d5782e11a917c47b4bc67d8c
Kashi1234 <kashikhan798@gmail.com>
2025-08-20 00:16:30 +0500

    Initial commit with .gitignore

---
+++ examples/ble_peripheral/ble_app_template/pca10056/s140/fitness_tracker/boards_custom.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "nrf_gpio.h"
+
+/* GPS UART pins */
+#define GPS_UART_TX 30
+#define GPS_UART_RX 31
+
+/* GPS control pins */
+#define PIN_GPS_PWR   15
+#define PIN_GPS_RST   23
+#define PIN_GPS_VBKUP 16
